68fef47066256e2e062f3d59cca31768ba5311d5
lwg <15983316702@163.com>
2019-06-25 23:58:07 +0800

    first commit

---
+++ src/main/java/com/cdzg/lwg/test2/Controller.java
@@ -0,0 +1,12 @@
+package com.cdzg.lwg.test2;
+
+/**
+ * @author lwg
+ * @Date: 2019/6/25 23:53
+ */
+public class Controller {
+    public static void main(String[] args) {
+
+        System.out.println("this is controller");
+    }
+}
